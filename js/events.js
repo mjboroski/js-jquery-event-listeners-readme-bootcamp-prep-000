73bcc8d770bdd211fd0ab5a927136950ad06e8e3
Michael Boroski <mjboroski@gmail.com>
2018-04-05 20:49:54 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert('Hey!');
+    return;
+  });
+}
+function frameIt(){
+  $('img').on("load", function(){
+    $('img.class')='.tasty'
+    return;
+  });
 
+}
+function pressIt(){
+  $('p').on("click", function(){
+    alert('Hey!');
+    return;
+  });
+}
+function submitIt(){
+  $('p').on("click", function(){
+    alert('Hey!');
+    return;
+  });
+}
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
