200eda5123e7ad6684130a0a036c8405d0cb7e84
Michael Boroski <mjboroski@gmail.com>
2018-04-05 21:02:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt(){
 }
 function pressIt(){
   $('#typing').on("keydown", function(key){
-    if(key===71){
+    if(key.which===71){
       alert('Hey! You pressed G!');
     }
     return;
