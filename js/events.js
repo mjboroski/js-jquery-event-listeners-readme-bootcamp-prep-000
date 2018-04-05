4908ea3872d70fc1b92f182d2bff75bb3f4f5eaa
Michael Boroski <mjboroski@gmail.com>
2018-04-05 21:03:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt(){
+  $('p').on("click", function(){
+    alert('Hey!');
+    return;
+  });
+}
+function frameIt(){
+  $('img').on("load", function(){
+    $('img').addClass('tasty');
+    return;
+  });
 
+}
+function pressIt(){
+  $('#typing').on("keydown", function(key){
+    if(key.which===71){
+      alert('Hey! You pressed G!');
+    }
+    return;
+  });
+}
+function submitIt(){
+  $('form').on("submit", function(){
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
 $(document).ready(function(){
-
-// call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
